3112a9c8d5ffa73ef83944dfb71c1a146f2c81d2
Jet-l <Sakurakmnt@163.com>
2021-07-03 14:59:08 +0800

    增删改查

--- mybatics01/src/main/java/com/dbly/dao/IStudentDAO.java
+++ mybatics01/src/main/java/com/dbly/dao/IStudentDAO.java
@@ -2,6 +2,15 @@ package com.dbly.dao;
 
 import com.dbly.entity.Student;
 
+import java.util.List;
+
 public interface IStudentDAO {
+
     void insertStudent(Student student);
+
+    List<Student> findAllStudent();
+
+    void deleteStudent(int id);
+
+    void updateStudent(Student student);
 }
--- mybatics01/src/test/java/com/dbly/JUnitTest.java
+++ mybatics01/src/test/java/com/dbly/JUnitTest.java
@@ -5,14 +5,18 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
 
 public class JUnitTest {
-    @Test
-    public void testStudent() throws IOException {
+    private SqlSession sqlSession;
+    @Before
+    public void init() throws IOException {
         //1.读取mybatis配置
         Reader reader = Resources.getResourceAsReader(
                 "Mapper/SqlMap.xml"
@@ -25,15 +29,45 @@ public class JUnitTest {
         SqlSessionFactory factory
                 = builder.build(reader);
         //4.获取Sqlsession对象
-        SqlSession sqlSession
-                = factory.openSession();
+        sqlSession = factory.openSession();
+    }
+    @Test
+    public void testStudent(){
         //5.通过session对象插入数据
         Student student = new Student();
-        student.setSname("阿瑟东");
+        student.setSname("gfdhb");
         student.setSsex("女");
         student.setClassName("东北林业大学");
         sqlSession.insert("insertStudent",student);
         sqlSession.commit();
+
+    }
+
+
+    @Test
+    public void testSelectStudent(){
+      List<Student> list = sqlSession.selectList("findAllStudent");
+      for (Student student : list){
+          System.out.println(student);
+      }
+    }
+
+    @Test
+    public void testDeleteStudent(){
+        sqlSession.delete("deleteStudent",1);
+        sqlSession.commit();
+    }
+    @Test
+    public void testUpdateStudent(){
+        Student student =new Student();
+        student.setSid(3);
+        student.setSname("sdasasf");
+        sqlSession.update("updateStudent",student);
+        sqlSession.commit();
+    }
+
+    @After
+    public void close(){
         //6.关闭session
         sqlSession.close();
     }
